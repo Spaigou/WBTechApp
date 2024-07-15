98476ebb1dae0faf31a1727d4601b797b74fe96d
Mirzakhanov Chingiz Ramilovich <chrmirzakhanov@sberdevices.ru>
2024-07-15 17:36:01 +0300

    lesson2 done

--- app/src/main/java/com/spaigou/wbtechandroidapp/components/atoms/Avatars.kt
+++ app/src/main/java/com/spaigou/wbtechandroidapp/components/atoms/Avatars.kt
@@ -12,12 +12,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
--- app/src/main/java/com/spaigou/wbtechandroidapp/lessons/SecondLesson.kt
+++ app/src/main/java/com/spaigou/wbtechandroidapp/lessons/SecondLesson.kt
@@ -1,8 +1,64 @@
 package com.spaigou.wbtechandroidapp.lessons
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.spaigou.wbtechandroidapp.components.Community
+import com.spaigou.wbtechandroidapp.components.CommunityCard
+import com.spaigou.wbtechandroidapp.components.Event
+import com.spaigou.wbtechandroidapp.components.EventCard
+import com.spaigou.wbtechandroidapp.components.VisitorsRow
+import com.spaigou.wbtechandroidapp.components.atoms.ProfileAvatar
+import java.time.LocalDate
 
 @Composable
 fun SecondLesson() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+    ) {
+        item {
+            val event = Event(
+                name = "Developer meeting",
+                date = LocalDate.of(2024, 9, 13),
+                city = "Москва",
+                categoryList = listOf("Python", "Junior", "Moscow")
+            )
+            EventCard(event)
+        }
+        item {
+            val event = Event(
+                name = "Developer meeting",
+                date = LocalDate.of(2024, 9, 13),
+                city = "Москва",
+                categoryList = listOf("Python", "Junior", "Moscow"),
+                isEnded = true
+            )
+            EventCard(event)
+        }
+        item {
+            val images = List(16) { "https://shorturl.at/ng9Tx" }
+            VisitorsRow(images)
+        }
+        item {
+            CommunityCard(community = Community("Designa", 10000))
+        }
+        item {
+            CommunityCard(community = Community("Designa", 1000))
+        }
+        item {
+            ProfileAvatar()
+            ProfileAvatar(editMode = true)
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun SecondLessonPreview() {
+    SecondLesson()
 }
