8e756094103c3d108894738ea0aa615691b1351f
Mirzakhanov Chingiz Ramilovich <chrmirzakhanov@sberdevices.ru>
2024-07-15 14:48:30 +0300

    visitors row implemented

---
+++ app/src/main/java/com/spaigou/wbtechandroidapp/components/atoms/VisitorsRow.kt
@@ -0,0 +1,57 @@
+package com.spaigou.wbtechandroidapp.components.atoms
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.spaigou.wbtechandroidapp.ui.theme.LocalTypography
+
+@Composable
+fun VisitorsRow(
+    images: List<String>,
+    modifier: Modifier = Modifier,
+    overlappingPercentage: Float,
+) {
+    val overlappingFactor = 1 - overlappingPercentage
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy((-48 * overlappingFactor).dp)
+    ) {
+        images.take(5).forEachIndexed { index, image ->
+            UserAvatar(
+                url = image,
+                modifier = modifier.zIndex(5 - index.toFloat()),
+                isVisitor = true
+            )
+        }
+        if (images.size > 5) {
+            Text(
+                text = "+${images.size - 5}",
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(start = (48 * overlappingFactor + 14).dp),
+                style = LocalTypography.current.bodyText1
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VisitorsRowPreview() {
+    LazyRow {
+        item {
+            val images = List(16) { "https://shorturl.at/ng9Tx" }
+            VisitorsRow(overlappingPercentage = 36.8f / 56, images = images)
+        }
+    }
+}
+
